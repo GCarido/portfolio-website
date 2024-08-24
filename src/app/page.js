81e92156c8bd6100d0f9c57ed39f0aee596e34a7
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-24 13:36:07 +0800

    Add homepage styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Hero
- from "./components/Hero";
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <Hero />
+      <div className="container mx-auto py-4 px-12">
+        <Hero />
+      </div>
     </main>
   );
 }
