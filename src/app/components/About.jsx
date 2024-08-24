b651bd12e498a874aaa32329084d912cca1f1785
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-24 15:24:18 +0800

    Add placeholder image in about section page

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import
+import React from 'react';
+import Image from 'next/image';
 
 const About = () => {
   return (
     <section className="text-white">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-
+            <Image src="/images/about-section.png" width={500} height={500} />
         </div>
     </section>
   )
